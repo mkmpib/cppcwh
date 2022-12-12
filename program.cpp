f7ebd320a90c1d784e605a1353aa79ba52ca99a0
manishmpib <manishmpib@gmail.com>
2022-12-12 21:09:46 +0530

    13

--- program.cpp
+++ program.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
     cout << "Hare kreshna HARE RAM";
-    cout<<"\nAcyuta Keshava Hari Narayana";
-    cout<<"enter num\n";
+    cout << "\nAcyuta Keshava  m Hari Narayana";
+    cout << "enter num\n";
     int n;
-    cin>>n;
-    cout<<n*n;
+    cin >> n;
+    cout << n * n;
     return 0;
 }
