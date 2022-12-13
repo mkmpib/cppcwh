82ec7269e73412373782f5fb419a392b0b966d0d
manishmpib <manishmpib@gmail.com>
2022-12-13 06:32:17 +0530

    tuesday13

---
+++ c programs/multipletable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int k;
+    for (int i = 11; i <= 18; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            k = i * j;
+            printf("%d\t", k);
+        }
+    }
+
+    return 0;
+}
